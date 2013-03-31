2dbda7fec013bb28f2c9fa291b6ca49bb7e671dc
Allison Heath <aheath@uchicago.edu>
2013-03-31 15:10:22 -0500

    Changes to make work on PDC

--- osdcquery/config/TcgaTest.py
+++ osdcquery/config/TcgaTest.py
@@ -12,11 +12,11 @@ fs_handler_class_name = 'UnixFsHandler'
 
 non_disease_dir = 'none'
 
-es_url = 'http://172.16.1.33:9200'
+es_url = 'http://172.16.1.3'
 es_index = 'tcga-cghub'
 es_doc_type = 'analysis'
 
-cdb_url = 'http://172.16.1.33:5984'
+cdb_url = 'https://172.16.1.3:6984'
 cdb_osdc = 'tcga-osdc'
 cdb_cghub = 'tcga-cghub'
 cdb_query = 'tcga-query'
